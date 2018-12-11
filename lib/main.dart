5c6259a5a9570c3cbabf6ccd71aed65cab614469
andreshg <andreshg@viridian.ltd>
2018-12-11 18:12:52 -0400

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:android_multiple_identifier/android_multiple_identifier.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _platformVersion = 'Unknown';
+  String _imei = 'Unknown';
+  String _serial = 'Unknown';
+  String _androidID = 'Unknown';
+
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    String imei;
+    String serial;
+    String androidID;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await AndroidMultipleIdentifier.platformVersion;
+      imei = await AndroidMultipleIdentifier.imeiCode;
+      serial = await AndroidMultipleIdentifier.serialCode;
+      androidID = await AndroidMultipleIdentifier.androidID;
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+      imei = 'Failed to get IMEI.';
+      serial = 'Failed to get Serial Code.';
+      androidID = 'Failed to get Android id.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+      _imei = imei;
+      _serial = serial;
+      _androidID = androidID;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+            child: Column(children: <Widget>[Text('\nRunning on: $_platformVersion\n'),
+            Text('IMEI: $_imei\n'),
+            Text('Serial Code: $_serial\n'),
+            Text('Android ID: $_androidID\n'),],)
+        ),
+      ),
+    );
+  }
+}
